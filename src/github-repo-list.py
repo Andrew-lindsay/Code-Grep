72c1038f0a48e1c85141d7f8b0ff9f275f7f3eec
Andrew-lindsay <andrewlindsay97@gmail.com>
2019-06-10 11:37:30 +0100

    Added function to built list of repos by iterating over star count

--- src/github-repo-list.py
+++ src/github-repo-list.py
@@ -16,7 +16,7 @@ def generate_repo_list(query_str):
         print("All search requested used up please wait 61s")
         sleep(61)
     res = g.search_repositories(query=query_str)
-    repo_res_list = open("repo_res_list.txt", "w")
+    repo_res_list = open("repo_res_list.txt", "a")
     print("Number of requests left: {}/{}"
           .format(g.get_rate_limit().search.remaining, g.get_rate_limit().search.limit))
     print("Total Count: {},  for query: {}".format(res.totalCount, query_str))
@@ -30,47 +30,31 @@ def generate_repo_list(query_str):
         if g.get_rate_limit().search.remaining == 0:
             # # DEBUG
             print("Number of requests left: {}/30"
-                .format(g.get_rate_limit().search.remaining))
+                  .format(g.get_rate_limit().search.remaining))
             # reset_time = g.rate_limiting_resettime - time() + 1
             # reset_time = reset_time if reset_time >= 0 else 1
             print("SLEEPING; Time left until reset: {}s".format(61))
             sleep(61)
             # DEBUG
             print("Number of requests left: {}/30"
-                .format(g.get_rate_limit().search.remaining))
+                  .format(g.get_rate_limit().search.remaining))
         for res_page in res.get_page(i):
             # print(res_page)
             repo_res_list.write(res_page.full_name + '\n')
         i += 1
-    print("FINISHED")
-    # try:
-    #     for res_page in res:
-    #         # print(res_page)
-    #         # DEBUG
-    #         # print("Number of requests left: {}/{}"
-    #                 # .format(g.get_rate_limit().search.remaining, g.get_rate_limit().search.limit))
-    #         # DEBUG
-    #         # print(g.get_rate_limit().search.reset)
-    #         # if no more requests can be made sleep for 60 seconds required reset time
-    #         if g.get_rate_limit().search.remaining == 0:
-    #             print("Number of requests left: {}/{}"
-    #                 .format(g.get_rate_limit().search.remaining, g.get_rate_limit().search.limit))
-    #             print(g.get_rate_limit().search.reset)
-    #             print("SLEEPING: Rate limiting sucks, Time left until reset: {}s".format(g.get_rate_limit().search.reset. - time()))
-    #             # sleep for require time not hardcoded 60
-    #             sleep(g.rate_limiting_resettime - time() + 4)
-    #         repo_res_list.write(res_page.full_name + '\n')
-    #         # repo_res_list.flush()
-
-    # except RateLimitExceededException as e:
-    #    print("Number of requests left: {}/{}"
-    #     .format(g.get_rate_limit().search.remaining, g.get_rate_limit().search.limit))
-
     print("Number of requests left: {}/{}"
           .format(g.get_rate_limit().search.remaining, g.get_rate_limit().search.limit))
+    print("FINISHED")
+
+
+def star_trawler(number_of_stars, start=0):
+    for stars in range(start, start + number_of_stars):
+        generate_repo_list(query_str="language:C++ stars:{}".format(stars))
+
 
 def main():
-    generate_repo_list(query_str="language:C stars:10")
+    # generate_repo_list(query_str="language:C++ stars:10")
+    star_trawler(number_of_stars=10, start=10)
 
 
 if __name__ == '__main__':
