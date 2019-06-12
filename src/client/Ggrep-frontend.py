a3edbb983e1fdefd6d6c8525f9978ffb0fc10edc
Andrew lindsay <Andrewlinday97@gmail.com>
2019-06-12 17:39:11 +0100

    attempt first go at multiline grep

--- src/client/Ggrep-frontend.py
+++ src/client/Ggrep-frontend.py
@@ -6,12 +6,20 @@ import livegrep_pb2
 import livegrep_pb2_grpc
 import os
 from os.path import join
+import re2
+import sys
 
+def goto_line_in_file(file_loc, line):
+    """ Returns location in file """
+    file_handle = open(file_loc, "r")
+    for num in xrange(line):
+        file_handle.readline()
+    return file_handle
 
-def server_call(query="", mutiline_quety="", matches_limit=50):
+def server_call(query="", multiline_query="", matches_limit=50):
     max_msg_len = 1024 * 1024 * 4  # 4MiB
     dir_loc = os.path.join(os.path.expanduser(
-        "~"), "Project/Code-grep/repo_pull_test8/repos")
+        "~"), "Projects/Code-Grep/repo_pull_test9/repos")
     with grpc.insecure_channel('localhost:9999', options=[('grpc.max_recieve_message_length', max_msg_len)]) as channel:
         stub = livegrep_pb2_grpc.CodeSearchStub(channel)
         response = stub.Search(livegrep_pb2.Query(
@@ -19,25 +27,36 @@ def server_call(query="", mutiline_quety="", matches_limit=50):
         # what is the response object can it be turned directly to json or python dict object
         # print(type(response))
         # print(response)
-        for item in response.results:
-            print("Path: {}/{}, line: {}".format(item.tree,
+        
+    query_re = re2.compile(query if multiline_query == "" else multiline_query, re2.M)
+    for item in response.results:
+        print("Path: {}/{}, line: {}".format(item.tree,
                                                  item.path, item.line_number))
-
-            file_loc = join(dir_loc, join(item.tree, item.path))
-            print(file_loc)
-
-            with open(file_loc, "r") as res_file:
-                print(res_file.readline)
-
-
-
-
+        print("\tcodeseach: {}".format(item.line))
+        line_num = item.line_number
+        file_loc = join(dir_loc, join(item.tree, item.path))
+        # print(file_loc)
+        
+        # go to specific line of file
+        
+        #with open(file_loc, "r") as res_file:
+        #    print(res_file.readline())
+        file_data = goto_line_in_file(file_loc, line_num-1) # at correct line
+        #print("\t at line: {}".format(file_data.readline()))
+        match_object = query_re.search(file_data.read())
+        
+        if match_object is not None:
+            print("\tString matched: {}".format(match_object.group()))
+        else:
+            print("No Match")
+        file_data.close()
+           
 def repos_returned():
     pass
 
 
 def main():
-    server_call(query="hello", matches_limit=4)
+    server_call(query=r"for\s*\(", multiline_query=r"for\s*\([^;]*;[^;]*;[^\)]*\)\s*{", matches_limit=4)
 
 
 if __name__ == '__main__':
