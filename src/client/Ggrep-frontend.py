5482ad99a0be480d98f7c6764f9db591e002120a
Andrew-lindsay <andrewlindsay97@gmail.com>
2019-06-14 14:16:45 +0100

    better approach to multi-grep added

--- src/client/Ggrep-frontend.py
+++ src/client/Ggrep-frontend.py
@@ -7,6 +7,7 @@ import livegrep_pb2_grpc
 import os
 from os.path import join
 import re2
+import pprint
 # import sys
 
 
@@ -17,6 +18,14 @@ def goto_line_in_file(file_loc, line):
         file_handle.readline()
     return file_handle
 
+
+def print_results(response):
+    for item in response.results:
+        print("{}/{}:{} {}".format(
+            item.tree.encode('ascii', 'ignore')
+            , item.path.encode('ascii', 'ignore')
+            , item.line_number
+            , item.line))
 
 def server_call(query="", matches_limit=50):
     # no effect due to server side
@@ -33,6 +42,39 @@ def server_call(query="", matches_limit=50):
         # directly to json or python dict object
         return response
 
+# parallelise, search of files
+def mgrep_in_files(multiline_query, cs_response):
+    """ Gather interesting files from initial response and grep them for results"""    
+
+    dir_loc = os.path.join(os.path.expanduser(
+        "~"), "Projects/test-env/repos")
+
+    query_re = re2.compile(multiline_query, re2.M)  # [^;] matches newlines
+
+    # file_name_path : hits in initial search
+    files_of_interest = {}
+    regex_hits = 0
+
+    for item in cs_response.results:
+        id_f = join(item.tree, item.path)
+
+        if id_f in files_of_interest:
+            files_of_interest[id_f] += 1
+        else:
+            files_of_interest[id_f] = 1
+
+            file_loc = join(dir_loc, id_f)
+
+            with open(file_loc, "r") as file_handle:
+                # res_q = query_re.finditer(file_handle.read())
+                # print(res_q)
+                for res_q in query_re.finditer(file_handle.read()):
+                    print("{}: {}".format(id_f, res_q.group()))
+                    regex_hits += 1
+
+    print("=== Number of responses: {} ===".format(regex_hits))
+    # pprint.pprint(files_of_interest)
+
 
 def mgrep(multiline_query, cs_response):
     """ preforms multiline grep over all files returned from initial search"""
@@ -42,22 +84,24 @@ def mgrep(multiline_query, cs_response):
     dir_loc = os.path.join(os.path.expanduser(
         "~"), "Projects/test-env/repos")
 
-    query_re = re2.compile(multiline_query, re2.M)
+    query_re = re2.compile(multiline_query, re2.M)  # [^;] matches newlines
 
     for item in cs_response.results:
-        print("{}/{}, line No: {}".format(
+        print("{}/{}:{} {}".format(
             item.tree.encode('ascii', 'ignore')
-            ,item.path.encode('ascii', 'ignore')
-            ,item.line_number))
+            , item.path.encode('ascii', 'ignore')
+            , item.line_number
+            , item.line))
 
-        print(item.line)
         line_num = item.line_number
         file_loc = join(dir_loc, join(item.tree, item.path))
-        # print(file_loc)
 
-        # go to specific line of file
+        # go to pecific line of filetr.partition(sep)
+        # is required ?
         file_data = goto_line_in_file(
             file_loc, line_num - 1)  # at correct line
+
+        # could start query at beginning of line to stop having to uses the scan method of search
 
         # print("\t at line: {}".format(file_data.readline()))
         # matches only next possible match to multi grep
@@ -65,14 +109,24 @@ def mgrep(multiline_query, cs_response):
         match_object = query_re.search(file_data.read())
 
         if match_object is not None:
-            print("\tString matched: {}".format(match_object.group()))
+            print("Matched: {}".format(match_object.group()))
         else:
             print("No Match")
         file_data.close()
 
 
 def prepocess_query(query_str):
+    # complete list of meta characters. ^ $ * + ? { } [ ] \ | ( )
+    # \number \A \B \b \D \d \S \s \W \w \Z
+    # standard escapes
+    # what about \n|^|$ at start of line (no query)
+    # being of query \n stops codesearch functioning
+    # special characters $|\n|^|.|*|()|
     codesearch_query = query_str
+    if "\\n" in query_str or '$' in query_str or '^' in query_str:
+        print("Query is multiline")
+        codesearch_query = query_str
+
     return (codesearch_query, query_str)
 
 
@@ -82,23 +136,26 @@ def g_grep(query="", multiline_q="", matches_limit=50):
     query_s, m_query = prepocess_query(query)
 
     res_response = server_call(query_s, matches_limit=matches_limit)
-
     print("=== initial search done ===")
+
     # save initial results
+    # print_results(response=res_response)
+
+    print("=== Number of responses: {} ===".format(len(list(res_response.results))))
 
     # see if multiline query can be build from query
     # i.e does query include multiline characters
     multiline_q = multiline_q if multiline_q != "" else m_query
 
-    results = mgrep(multiline_q, res_response)
-
+    # results = mgrep(multiline_q, res_response)
+    mgrep_in_files(multiline_q, res_response)
     # save results/ output
 
 
 def main():
     # handle args and other stuff here
-    g_grep(query=r"for\s*\(",
-           multiline_q=r"for\s*\([^;]*;[^;]*;[^\)]*\)\s*{", matches_limit=4)
+    g_grep(query=r"int",
+           multiline_q=r"int", matches_limit=10000)
 
 
 if __name__ == '__main__':
