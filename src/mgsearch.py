c3c1733c48e19f1483565cfff22f5122c2191d3d
Andrew-lindsay <andrewlindsay97@gmail.com>
2019-06-19 14:40:59 +0100

    Used process pool to parallelize grep

--- src/mgsearch.py
+++ src/mgsearch.py
@@ -2,6 +2,36 @@
 
 import re2
 import os
+import sys
+import time
+from multiprocessing import Process, Pool, Manager
+
+
+def pool_multiproc(query, nprocs=4):
+    """ requires reading/gathering all directories at once"""
+    p = Pool(processes=nprocs)
+
+    # Using file to pass files name to grep
+    with open("file_names.txt", "r") as fn:
+        file_names = fn.readlines()
+    # =====================================
+
+    res = p.map(worker, [(query, f) for f in file_names])
+    print("=== Number of hits: {} ===".format(sum(res)))
+
+
+def worker(data):
+    query, file_name = data
+    query_re = re2.compile(query, re2.MULTILINE)
+    regex_hits = 0
+
+    with open(file_name.strip('\n'), "r") as file_handle:
+        for res_q in query_re.finditer(file_handle.read()):
+            print("{}: {}".format(file_name.strip('\n'), res_q.group()))
+            regex_hits += 1
+
+    # print("=== Number of hits: {} ===".format(regex_hits))
+    return regex_hits
 
 
 def mgsearch(query):
@@ -20,7 +50,12 @@ def mgsearch(query):
 
 
 def main():
-    mgsearch(query="std::.*::iterator")
+    """ Handles passing of args to function """
+    query_arg = sys.argv[1]
+    print(sys.argv)
+    # mgsearch(query=query_arg)
+    # =====================================
+    pool_multiproc(query_arg, nprocs=4)
 
 
 if __name__ == '__main__':
