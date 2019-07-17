8ff24dea0d9a1fde783d82ead32d8b42ba6ab2ad
Andrew-lindsay <andrewlindsay97@gmail.com>
2019-07-17 18:08:49 +0100

    Added new transformation check method and command line interface for transformApplication.py

--- src/transformApplication.py
+++ src/transformApplication.py
@@ -7,6 +7,8 @@ import json
 from shutil import copyfile, copy2, rmtree
 import re2
 import csv
+import difflib
+import argparse
 
 
 def get_includes(repo_loc):
@@ -32,7 +34,7 @@ def compile_transformed(repo_loc, include_list, input_f, output_f):
     command_c = ["g++-6", "-std=c++17", "-c", input_f, "-o", output_f + ".o"]
     command_c.extend(include_list)
     # print(' '.join(command_c))
-    p = Popen(command_c, stdout=nul, stderr=None)
+    p = Popen(command_c, stdout=nul, stderr=nul)
     p.wait()
 
     print("Compilation return code: {}".format(p.returncode))
@@ -46,7 +48,7 @@ def compile_transformed(repo_loc, include_list, input_f, output_f):
 def apply_transformation(transform_tool, regex_hit_pattern, input_f, output_f):
     nul = open(os.devnull, 'w')
     # print("tool: {}\ninput: {}\nouput: {}".format(' '.join(transform_tool), input_f, output_f))
-    # print(' '.join(transform_tool))
+    print(' '.join(transform_tool))
     counter = 0
 
     p = Popen(transform_tool, stdout=PIPE, stderr=None)
@@ -66,7 +68,7 @@ def apply_transformation(transform_tool, regex_hit_pattern, input_f, output_f):
     print("Number of successful transformations: {}".format(counter))
     nul.close()
 
-    return counter if p.returncode == 0 else -1 
+    return counter if p.returncode == 0 else -1
 
 
 def get_input_output_loc(transform_tool):
@@ -110,7 +112,23 @@ def remove_transform_dir(repo_dir="repos", results_dict={}, copy_req=True):
             print("Build directory already exists: {}".format(build_path))
 
 
-def transform_files(transform_tool, repo_dir="repos", results_dict={}, copy_req=True):
+def num_diff_regions(before, after):
+    before_f = open(before, "r")
+    after_f = open(after, "r")
+    counter = 0
+
+    for line in difflib.unified_diff(before_f.readlines(), after_f.readlines(), n=0):
+        if line.startswith("@@"):
+            counter += 1
+
+    before_f.close()
+    after_f.close()
+
+    print("Number of continues changed chunks: {}".format(counter))
+    return counter
+
+
+def transform_files(transform_tool, regex, output_file="transform_results.csv", repo_dir="repos", results_dict={}, copy_req=True):
 
     global succ_comps
     succ_comps = 0
@@ -124,6 +142,8 @@ def transform_files(transform_tool, repo_dir="repos", results_dict={}, copy_req=
 
     csv_results_data = open("transform_results.csv", "w")
     csv_writer = csv.writer(csv_results_data, delimiter='\t')
+    csv_writer.writerow(
+        ["Repo Name", "File Name", "Diff chunks", "Transformed", "Compilation"])
 
     # get results
     for repo_name, file_list in results_dict.iteritems():
@@ -148,17 +168,19 @@ def transform_files(transform_tool, repo_dir="repos", results_dict={}, copy_req=
 
         for file_n in file_list:
             # transform file
-            file_path = join(repo_loc, file_n)
+            in_file = join(repo_loc, file_n)
             out_file = join(build_path, basename(file_n))
             total_number += 1
 
             print("\n=====Process File======")
-            print(file_path)
+            print(in_file)
 
             # clang-tidy -checks='modernize-loop-convert' file.in -- -std=c++11
             if copy_req:
-                copyfile(file_path, out_file)
+                copyfile(in_file, out_file)
                 file_path = out_file
+            else:  # clang applies changes in place
+                file_path = in_file
 
             # what if tool requires -I arguements to work
             # clang could be used on set of files at once but not all tools could do this
@@ -170,7 +192,7 @@ def transform_files(transform_tool, repo_dir="repos", results_dict={}, copy_req=
                 transform_tool, r"modernize-loop-convert", input_f=file_path, output_f=out_file)
             # print("Transformation Complete")
 
-            if transform_c  != -1:
+            if transform_c != -1:
                 transform_count += transform_c
 
             #  here
@@ -178,13 +200,42 @@ def transform_files(transform_tool, repo_dir="repos", results_dict={}, copy_req=
             comp_return_code = compile_transformed(repo_loc, include_list,
                                                    input_f=file_path, output_f=out_file)
 
+            diff_chunk_count = -1
+            if os.path.isfile(out_file):
+                diff_chunk_count = num_diff_regions(
+                    before=in_file, after=out_file)
+
             csv_writer.writerow(
-                [repo_name, file_n, transform_c if transform_c != -1 else "FAILURE", "SUCCESSFUL" if comp_return_code == 0 else "FAILURE"])
+                [repo_name,
+                 file_n,
+                 diff_chunk_count,
+                 transform_c if transform_c != -1 else "FAILURE",
+                 "SUCCESSFUL" if comp_return_code == 0 else "FAILURE"])
 
     print("TOTAL SUCCESSFUL COMPILATIONS: {}/{}".format(succ_comps, total_number))
     print("TOTAL NUMBER OF TRANSFORMATIONS: {}".format(transform_count))
     # all files done now compile directory
     # compile_transformed_files(repo_loc, make_file_loc="~/Project/Code-Grep/Makefile")
+
+
+def parse_args():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        '--directory', '-d', help="directory containing repositories", default="repos")
+    arg_parser.add_argument(
+        '--tool', '-t', help="The command line tool and its arguements to run", type=str, required=True)
+    arg_parser.add_argument(
+        '--regex', '-r', help="Regex pattern to detect hits in tools standard output", default="", type=str)
+    arg_parser.add_argument(
+        '--input_file', '-f', help="Json file returned by mgsearch to what files to transform", default="results.json")
+    arg_parser.add_argument(
+        '--output_csv', '-o', help="Name for csv produced as output", default="transform_results.csv")
+    arg_parser.add_argument(
+        '--clean', '-cl', action='store_true', default=False,
+        help="Remove all build directories in repos specified by input file that were created by this tool, use before issuing a second search query over repositories")
+
+    parsed = arg_parser.parse_args()
+    return parsed.directory, parsed.tool, parsed.regex, parsed.input_file, parsed.output_csv, parsed.clean
 
 
 def main():
@@ -196,23 +247,27 @@ def main():
     # wait until search completes ?
     # direct use of gcc to compile :-)
 
-    with open("results.json", "r") as query_results:
+    directory, tool, regex, input_file, output_csv, clean = parse_args()
+
+    with open(input_file, "r") as query_results:
         results_dict = json.load(query_results)
 
-    repo_dir = "repos"
-
     #  How to know where to place args in command
-    try:
-        if sys.argv[1] == "clean":
-            remove_transform_dir(repo_dir="repos", results_dict=results_dict)
-            return
-    except IndexError:
-        pass
+
+    if clean:
+        remove_transform_dir(repo_dir=directory, results_dict=results_dict)
+        return
 
     transform_files(
-        transform_tool=[
-            "clang-tidy", "-checks=-*,modernize-loop-convert", 'input', "--", "-std=c++14"],
-        repo_dir=repo_dir, results_dict=results_dict)
+        transform_tool=tool.split(" "), regex=regex,
+        output_file=output_csv, repo_dir=directory,
+        results_dict=results_dict)
+
+    # transform_files(
+    #     transform_tool=[
+    #         "clang-tidy", "-checks=-*,modernize-loop-convert", "--fix-errors", 'input', "--", "-std=c++14"],
+    #     regex="modernize-loop-convert",
+    #     repo_dir=repo_dir, results_dict=results_dict)
 
 
 if __name__ == '__main__':
